460fd64ffbedd980cc68894fa77cf63e3d347080
Mariusz G <gunio2322@gmail.com>
2022-02-20 20:53:18 +0100

    clean

--- hil222.js
+++ hil222.js
@@ -1,88 +1,52 @@
-(function() {
-
-let callback = (entries, observer) => {
+(function () {
+  let callback = (entries, observer) => {
     entries.forEach((entry) => {
-// console.log(entry)
-     
-     if(entry.isIntersecting){
-       console.log(entry.target.id);
-      //  let manh = entry.target;
-      //  console.log(manh);
-//  let bab = manh.innerHTML      
-
-// console.log(manh.attr("#"));
-
-       
-     
-    }
-    // else if(entry.target){
-    //   console.log('isinter');
-
-    //   }
-    })};
-  
+      if (entry.isIntersecting) {
+        console.log(entry.target.id)
+      }
+    })
+  }
   let options = {
     root: document.querySelector('#header'),
     rootMargin: '0px',
-    threshold: 0.6
-    }; 
-let observer = new IntersectionObserver(callback, options);
-// const target = document.querySelector('#containers');
-// const arow = document.querySelectorAll('a[href^="#"]');
+    threshold: 0.6,
+  }
+  let observer = new IntersectionObserver(callback, options)
+  let targets = document.querySelectorAll('section[id]')
+  targets.forEach(function (e) {
+    observer.observe(e)
+  })
+  let navia = document.querySelectorAll('[href^="#"]')
+  navia.forEach(function (n) {
+    let aga = n.innerHTML
 
-let targets = document.querySelectorAll('section[id]');
-targets.forEach(function(e){
-  observer.observe(e)
-  // console.log(e);
-  // console.log(targets);
-});
-let navia = document.querySelectorAll('[href^="#"]');
-navia.forEach(function(n){
-  console.log(n);
-let aga = n.innerHTML;
-
-console.log(aga);
-
-
-
-});
-  
-
-
-// conty.forEach(function(entry) {
-//   console.log(entry);
-// });
-
-// observer.observe(target)
-
-}());
+    console.log(aga)
+  })
+})();
 // DRUGI OBSERVER
 
+// (function () {
+//   let callback = (entries, observer) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         console.log(entry.target.id)
+//       }
+//     })
+//   }
+//   let options = {
+//     root: document.querySelector('#header'),
+//     rootMargin: '0px',
+//     threshold: 0.6,
+//   }
+//   let observer = new IntersectionObserver(callback, options)
+//   let targets = document.querySelectorAll('section[id]')
+//   targets.forEach(function (e) {
+//     observer.observe(e)
+//   })
+//   let navia = document.querySelectorAll('[href^="#"]')
+//   navia.forEach(function (n) {
+//     let aga = n.innerHTML
 
-// (function() {
-//     let callback = (entries, observer) => {
-//         entries.forEach(entry => {
-//           if(entry.isIntersecting){
-        
-//          document.querySelector('header').classList.remove('opaque'); 
-//       //    document.querySelector('body').classList.remove('supka'); 
-//           }
-//           else if(entry.target){
-//             document.querySelector('header').classList.add('opaque'); 
-//           //   document.querySelector('body').classList.add('supka'); 
-//         } 
-//       })}
-//         let options = {
-         
-//           root: null, //document.querySelector('#jobs'),
-//           rootMargin: '0px',
-//           threshold: 1.0
-          
-//         };
-        
-    
-//       let observer = new IntersectionObserver(callback, options);
-//       let target = document.querySelector('#start');
-//       observer.observe(target); //target moze byc img div
-  
-//   }());
+//     console.log(aga)
+//   })
+// })()
