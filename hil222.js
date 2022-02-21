e72b7927de25738022c1c90857198ebd28715620
Mariusz G <gunio2322@gmail.com>
2022-02-21 08:22:51 +0100

    code

--- hil222.js
+++ hil222.js
@@ -1,28 +1,38 @@
-(function () {
-  let callback = (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        console.log(entry.target.id)
-      }
-    })
-  }
-  let options = {
-    root: document.querySelector('#header'),
-    rootMargin: '0px',
-    threshold: 0.6,
-  }
-  let observer = new IntersectionObserver(callback, options)
-  let targets = document.querySelectorAll('section[id]')
-  targets.forEach(function (e) {
-    observer.observe(e)
-  })
-  let navia = document.querySelectorAll('[href^="#"]')
-  navia.forEach(function (n) {
-    let aga = n.innerHTML
+let callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.id
+      console.log(entry.target.id)
+    }
 
-    console.log(aga)
+    // let idname = entry.target.id;
+    // console.log(idname);
   })
-})();
+}
+
+let options = {
+  root: document.querySelector('#header'),
+  rootMargin: '0px',
+  threshold: 0.6,
+}
+let observer = new IntersectionObserver(callback, options)
+let targets = document.querySelectorAll('section[id]')
+targets.forEach(function(e) {
+  observer.observe(e)
+})
+
+let navia = document.querySelectorAll('[href^="#"]')
+navia.forEach(function(n) {
+  var nin = n.innerHTML
+  console.log(nin);
+})
+
+// let nun = navia.innerHTML
+// console.log(nun);
+
+
+// console.log(navia);
+
 // DRUGI OBSERVER
 
 // (function () {
