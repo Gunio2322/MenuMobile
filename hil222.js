fa83665caf18d84a771b2b5a7edf77eef8c1d009
Mariusz G <gunio2322@gmail.com>
2022-03-01 19:40:31 +0100

    ok?

--- hil222.js
+++ hil222.js
@@ -1,75 +1,29 @@
-// ma pobrac to co widzi aktualnie observer.Observer ma zwrucic href - bierzacy href ma odpowiadac id w menubar.
-
 let callback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       let id_ent = entry.target.id
       let hash = '#'
       var hash_id = hash.concat(id_ent)
-
-      // console.log(typeof hash_id)
-      console.log(hash_id)
-
       var navi = document
-        .querySelectorAll('a[href^="#"]')
-        .forEach((linkName) => {
-          var hr = linkName.getAttribute('href') 
-          
-          if(
-          hr === hash_id){
-console.log(linkName.hr)
-hr.classList.add('current')
-}
-          // 'href'.classList.add('current')
-          // console.log(linkName)
+        .querySelectorAll('a')
+        .forEach((link_name) => {
+          var href = link_name.getAttribute('href')
+          if (href === hash_id) {
+            link_name.classList.add('current')
+          } else if (href !== hash_id) {
+            link_name.classList.remove('current')
+          }
         })
     }
   })
 }
-
 let options = {
   root: document.querySelector('#header'),
   rootMargin: '0px',
-  threshold: 0.6,
+  threshold: 0.8,
 }
 observer = new IntersectionObserver(callback, options)
 let targets = document.querySelectorAll('section[id]')
 targets.forEach((target) => {
   observer.observe(target)
 })
-
-// let navi = document.querySelectorAll('a[href^="#"]')
-// navi.forEach((l) => {
-//   let inn = l.id
-
-//   console.log(inn)
-//   // console.log(navi)
-// })
-
-// DRUGI OBSERVER
-
-// (function () {
-//   let callback = (entries, observer) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         console.log(entry.target.id)
-//       }
-//     })
-//   }
-//   let options = {
-//     root: document.querySelector('#header'),
-//     rootMargin: '0px',
-//     threshold: 0.6,
-//   }
-//   let observer = new IntersectionObserver(callback, options)
-//   let targets = document.querySelectorAll('section[id]')
-//   targets.forEach(function (e) {
-//     observer.observe(e)
-//   })
-//   let navia = document.querySelectorAll('[href^="#"]')
-//   navia.forEach(function (n) {
-//     let aga = n.innerHTML
-
-//     console.log(aga)
-//   })
-// })()
