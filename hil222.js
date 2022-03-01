47901151f30e61f6ebcad0ff63130f1938bfc3cf
Mariusz G <gunio2322@gmail.com>
2022-03-01 06:35:14 +0100

    concat

--- hil222.js
+++ hil222.js
@@ -1,12 +1,29 @@
+// ma pobrac to co widzi aktualnie observer.Observer ma zwrucic href - bierzacy href ma odpowiadac id w menubar.
+
 let callback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.id
-      console.log(entry.target.id)
-    }
+      let id_ent = entry.target.id
+      let hash = '#'
+      var hash_id = hash.concat(id_ent)
 
-    // let idname = entry.target.id;
-    // console.log(idname);
+      // console.log(typeof hash_id)
+      console.log(hash_id)
+
+      var navi = document
+        .querySelectorAll('a[href^="#"]')
+        .forEach((linkName) => {
+          var hr = linkName.getAttribute('href') 
+          
+          if(
+          hr === hash_id){
+console.log(linkName.hr)
+hr.classList.add('current')
+}
+          // 'href'.classList.add('current')
+          // console.log(linkName)
+        })
+    }
   })
 }
 
@@ -15,23 +32,19 @@ let options = {
   rootMargin: '0px',
   threshold: 0.6,
 }
-let observer = new IntersectionObserver(callback, options)
+observer = new IntersectionObserver(callback, options)
 let targets = document.querySelectorAll('section[id]')
-targets.forEach(function(e) {
-  observer.observe(e)
+targets.forEach((target) => {
+  observer.observe(target)
 })
 
-let navia = document.querySelectorAll('[href^="#"]')
-navia.forEach(function(n) {
-  var nin = n.innerHTML
-  console.log(nin);
-})
+// let navi = document.querySelectorAll('a[href^="#"]')
+// navi.forEach((l) => {
+//   let inn = l.id
 
-// let nun = navia.innerHTML
-// console.log(nun);
-
-
-// console.log(navia);
+//   console.log(inn)
+//   // console.log(navi)
+// })
 
 // DRUGI OBSERVER
 
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // SmoothScroll
 ;(function () {
-  document.querySelectorAll('a[href^="#"]').forEach((elem) => {
+  // pobier wszystkie linki a href z menu i z foreach rozdziel
+  document.querySelectorAll('a[href^="#"]').forEach((elem) => { 
+    // na kazdm pojedynczym elemencie z menu  nasłuchuj click i wykonaj funkje z arg (e)
     elem.addEventListener('click', (e) => {
       e.preventDefault()
+      // na elemencie elem z pobranym  atrybutem 'href' wykonaj metode scrollIntoView()
       document.querySelector(elem.getAttribute('href')).scrollIntoView({
         behavior: 'smooth',
         offsetTop: 80,
