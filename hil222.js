578e6d13bc1b3b6511252b7635bbbbdf2ddf9219
Mariusz G <gunio2322@gmail.com>
2022-03-03 17:59:14 +0100

    szablon wyjsciowy

--- hil222.js
+++ hil222.js
@@ -3,17 +3,15 @@ let callback = (entries, observer) => {
     if (entry.isIntersecting) {
       let id_ent = entry.target.id
       let hash = '#'
-      var hash_id = hash.concat(id_ent)
-      var navi = document
-        .querySelectorAll('a')
-        .forEach((link_name) => {
-          var href = link_name.getAttribute('href')
-          if (href === hash_id) {
-            link_name.classList.add('current')
-          } else if (href !== hash_id) {
-            link_name.classList.remove('current')
-          }
-        })
+      let hash_id = hash.concat(id_ent)
+      let navi = document.querySelectorAll('a').forEach((link_name) => {
+        let href = link_name.getAttribute('href')
+        if (href === hash_id) {
+          link_name.classList.add('current')
+        } else if (href !== hash_id) {
+          link_name.classList.remove('current')
+        }
+      })
     }
   })
 }
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // SmoothScroll
+
 ;(function () {
-  // pobier wszystkie linki a href z menu i z foreach rozdziel
-  document.querySelectorAll('a[href^="#"]').forEach((elem) => { 
-    // na kazdm pojedynczym elemencie z menu  nasłuchuj click i wykonaj funkje z arg (e)
+  document.querySelectorAll('a[href^="#"]').forEach((elem) => {
     elem.addEventListener('click', (e) => {
       e.preventDefault()
-      // na elemencie elem z pobranym  atrybutem 'href' wykonaj metode scrollIntoView()
       document.querySelector(elem.getAttribute('href')).scrollIntoView({
         behavior: 'smooth',
         offsetTop: 80,
@@ -18,10 +16,40 @@
 (function () {
   const navithem = document.querySelector('ul')
   const burger = document.querySelector('.hamburger')
-  burger.addEventListener('click', function(){
+  burger.addEventListener('click', function () {
     navithem.classList.toggle('active')
-    navithem.addEventListener('click', function(){
+    navithem.addEventListener('click', function () {
       navithem.classList.remove('active')
     })
+  })
+})();
+// Menu current
+(function () {
+  let callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        let id_ent = entry.target.id
+        let hash = '#'
+        let hash_id = hash.concat(id_ent)
+        let navi = document.querySelectorAll('a').forEach((link_name) => {
+          let href = link_name.getAttribute('href')
+          if (href === hash_id) {
+            link_name.classList.add('current')
+          } else if (href !== hash_id) {
+            link_name.classList.remove('current')
+          }
+        })
+      }
+    })
+  }
+  let options = {
+    root: document.querySelector('#header'),
+    rootMargin: '0px',
+    threshold: 0.8,
+  }
+  observer = new IntersectionObserver(callback, options)
+  let targets = document.querySelectorAll('section[id]')
+  targets.forEach((target) => {
+    observer.observe(target)
   })
 })();
